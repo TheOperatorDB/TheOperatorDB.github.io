3de0bb40206f942e7f541bb711a685fafc576453
Flavien <flavien.sellet@gmail.com>
2024-07-31 17:49:33 +0200

    Styling, files architecture, missing HumanDB entries, documents data logic and filters rework

--- js/carDB.js
+++ js/carDB.js
@@ -1,66 +1,73 @@
 function fillTable(cars) {
-    var table = document.getElementById('carDB');
+  var table = document.getElementById("carDB");
+  let imgPath =
+    "https://raw.githubusercontent.com/TheOperatorDB/TheOperatorDB.github.io/main/images/carDB/";
 
-    cars.sort(function (a, b) {
-        return a.name.localeCompare(b.name);
-    });
+  cars.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
 
-    for (var i = 0; i < cars.length; i++) {
-        let row = document.createElement("tr");
+  for (var i = 0; i < cars.length; i++) {
+    let row = document.createElement("tr");
+    row.classList.add("align-middle");
 
-        let img = document.createElement("td");
-        img.innerHTML = cars[i].img === undefined ? "" : "<button class=\"btn\" onclick=\"window.open('https://raw.githubusercontent.com/TheOperatorDB/TheOperatorDB.github.io/main/images/carDB/" + cars[i].img + "','_blank')\"><img class=\"imgIcon\" src=\"images/carDB/imageIcon.png\" width=\"20px\"></button>";
-        row.appendChild(img);
+    let img = document.createElement("td");
+    img.innerHTML =
+      cars[i].img === undefined
+        ? ""
+        : '<button class="btn" onclick="window.open(\'' +
+          imgPath +
+          cars[i].img +
+          '\',\'_blank\')"><img class="imgIcon" src="/images/carDB/imageIcon.png" width="20px"></button>';
+    row.appendChild(img);
 
-        let plate = document.createElement("td");
-        plate.innerText = cars[i].plate;
-        row.appendChild(plate);
+    let plate = document.createElement("td");
+    plate.innerText = cars[i].plate;
+    row.appendChild(plate);
 
-        let status = document.createElement("td");
-        status.innerText = cars[i].status;
-        row.appendChild(status);
-        
-        let fabDate = document.createElement("td");
-        fabDate.innerText = cars[i].fabDate;
-        row.appendChild(fabDate);
+    let status = document.createElement("td");
+    status.innerText = cars[i].status;
+    row.appendChild(status);
 
-        let color = document.createElement("td");
-        color.innerText = cars[i].color;
-        row.appendChild(color);
+    let fabDate = document.createElement("td");
+    fabDate.innerText = cars[i].fabDate;
+    row.appendChild(fabDate);
 
-        let model = document.createElement("td");
-        model.innerText = cars[i].model;
-        row.appendChild(model);
+    let color = document.createElement("td");
+    color.innerText = cars[i].color;
+    row.appendChild(color);
 
-        let owner = document.createElement("td");
-        owner.innerText = cars[i].knownFacts.owner;
-        row.appendChild(owner);
+    let model = document.createElement("td");
+    model.innerText = cars[i].model;
+    row.appendChild(model);
 
-        let distributor = document.createElement("td");
-        distributor.innerText = cars[i].knownFacts.distributor;
-        row.appendChild(distributor);
+    let owner = document.createElement("td");
+    owner.innerText = cars[i].knownFacts.owner;
+    row.appendChild(owner);
 
-        let dateOfPurchase = document.createElement("td");
-        dateOfPurchase.innerText = cars[i].knownFacts.dateOfPurchase;
-        row.appendChild(dateOfPurchase);
+    let distributor = document.createElement("td");
+    distributor.innerText = cars[i].knownFacts.distributor;
+    row.appendChild(distributor);
 
-        let stolen = document.createElement("td");
-        stolen.innerText = cars[i].knownFacts.stolen;
-        row.appendChild(stolen);
+    let dateOfPurchase = document.createElement("td");
+    dateOfPurchase.innerText = cars[i].knownFacts.dateOfPurchase;
+    row.appendChild(dateOfPurchase);
 
-        table.appendChild(row);
-    }
+    let stolen = document.createElement("td");
+    stolen.innerText = cars[i].knownFacts.stolen;
+    row.appendChild(stolen);
+
+    table.appendChild(row);
+  }
 }
 
-function fetchJson(path) {
-    fetch(path)
-    .then(response => response.json())
-    .then(json => fillTable(json.d))
-    .catch(error => console.error('Error fetching JSON:', error))
+function fetchCars() {
+  var path = "/data/carDB.json";
+
+  fetch(path)
+    .then((response) => response.json())
+    .then((json) => fillTable(json.d))
+    .catch((error) => console.error("Error fetching JSON:", error));
 }
 
-function displayCarDB() {
-    fetchJson('data/carDB.json');
-}
-
-displayCarDB();
+fetchCars();
--- js/humanDB.js
+++ js/humanDB.js
@@ -1,177 +1,215 @@
 function fillTable(humans, filters) {
-    var table = document.getElementById('humanDB');
-    table.innerHTML = '';
+  var table = document.getElementById("humanDB");
+  table.innerHTML = "";
 
+  var imgPath =
+    "https://raw.githubusercontent.com/TheOperatorDB/TheOperatorDB.github.io/main/images/humanDB/";
 
-    var humans = humans.filter(function (human, index) {
-        if (filters.search != '') {
-            return human.name.toLowerCase().includes(filters.search.toLowerCase());
-        }
-        return true;
-    });
-
-    var humans = humans.filter(function (human, index) {
-        if (filters.sex != '') {
-            return human.sex.toLowerCase() == filters.sex;
-        }
-
-        return true;
-    });
-
-    var humans = humans.filter(function (human, index) {
-        if (filters.skin != '') {
-            return human.skin.toLowerCase() == filters.skin;
-        }
-
-        return true;
-    });
-
-    var humans = humans.filter(function (human, index) {
-        if (filters.eyeColor != '') {
-            return human.eyeColor.toLowerCase() == filters.eyeColor;
-        }
-
-        return true;
-    });
-
-    var humans = humans.filter(function (human, index) {
-        if (filters.criminalRecord == 'yes') {
-            return human.criminalRecord.length > 0;
-        } else if (filters.criminalRecord == 'no') {
-            return human.criminalRecord.length == 0;
-        }
-
-        return true;
-    });
-
-    var humans = humans.filter(function (human, index) {
-        if (filters.fingerprints == 'yes') {
-            return human.fingerprints != undefined;
-        } else if (filters.fingerprints == 'no') {
-            return human.fingerprints == undefined;
-        }
-
-        return true;
-    });
-    
-    var humans = humans.filter(function (human, index) {
-        if (filters.status != '' && human.status != undefined) {
-            return human.status.toLowerCase() == filters.status;
-        }
-
-        return true;
-    });
-
-    humans.sort(function (a, b) {
-        return a.name.localeCompare(b.name);
-    });
-
-    for (var i = 0; i < humans.length; i++) {
-        let row = document.createElement("tr");
-        
-        let img = document.createElement("td");
-        img.innerHTML = humans[i].img === undefined ? "" : "<button class=\"btn\" onclick=\"window.open('https://raw.githubusercontent.com/TheOperatorDB/TheOperatorDB.github.io/main/images/humanDB/" + humans[i].img + "','_blank')\"><img class=\"imgIcon\" src=\"images/humanDB/imageIcon.png\"></button>";
-        row.appendChild(img);
-
-        let name = document.createElement("td");
-        name.classList.add("name");
-        name.innerText = humans[i].name;
-        row.appendChild(name);
-
-        let sex = document.createElement("td");
-        sex.classList.add("sex");
-        sex.innerText = humans[i].sex === undefined ? "" : humans[i].sex;
-        row.appendChild(sex);
-        
-        let age = document.createElement("td");
-        age.classList.add("age");
-        age.innerText = humans[i].age === undefined ? "" : humans[i].age;
-        row.appendChild(age);
-
-        let skin = document.createElement("td");
-        skin.classList.add("skin");
-        skin.innerText = humans[i].skin === undefined ? "" : humans[i].skin;
-        row.appendChild(skin);
-
-        let eyeColor = document.createElement("td");
-        eyeColor.classList.add("eyeColor");
-        eyeColor.innerText = humans[i].eyeColor === undefined ? "" : humans[i].eyeColor;
-        row.appendChild(eyeColor);
-
-        let height = document.createElement("td");
-        height.classList.add("height");
-        height.innerText = humans[i].height === undefined ? "" : humans[i].height;
-        row.appendChild(height);
-
-        let weight = document.createElement("td");
-        weight.classList.add("weight");
-        weight.innerText = humans[i].weight === undefined ? "" : humans[i].weight;
-        row.appendChild(weight);
-        
-        let criminalRecord = document.createElement("td");
-        criminalRecord.classList.add("criminalRecord");
-        if (humans[i].criminalRecord.length === 0) {
-            criminalRecord.classList.add("green");
-            criminalRecord.innerText = "No";
-        } else {
-            criminalRecord.classList.add("red");
-            criminalRecord.innerText = "Yes";
-        }
-        row.appendChild(criminalRecord);
-
-        let fingerprints = document.createElement("td");
-        fingerprints.classList.add("fingerprints");
-        if (humans[i].fingerprints === undefined) {
-            fingerprints.innerText = "No";
-        } else {
-            fingerprints.classList.add("red");
-            fingerprints.innerText = "Yes";
-        }
-        row.appendChild(fingerprints);
-
-        let status = document.createElement("td");
-        status.classList.add("status");
-        status.innerText = humans[i].status;
-        switch (humans[i].status) {
-            case 'Unknown':
-                status.classList.add('orange');
-                break;
-            case 'Dead':
-                status.classList.add('red');
-                break;
-            case 'Alive':
-                status.classList.add('green');
-                break;
-            case undefined:
-                status.innerText = '';
-                status.classList.add('orange');
-                break;
-            default:
-                break;
-        }
-        row.appendChild(status);
-
-        table.appendChild(row);
+  var humans = humans.filter(function (human, index) {
+    if (filters.search != "") {
+      return human.name.toLowerCase().includes(filters.search.toLowerCase());
     }
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.sex != "") {
+      return human.sex.toLowerCase() == filters.sex;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.skin != "") {
+      return human.skin.toLowerCase() == filters.skin;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.eyeColor != "") {
+      return human.eyeColor.toLowerCase() == filters.eyeColor;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (
+      filters.bloodType == "unknown" &&
+      human.knownFacts.bloodType == undefined
+    ) {
+      return true;
+    } else if (
+      filters.bloodType != "" &&
+      human.knownFacts.bloodType != undefined
+    ) {
+      return human.knownFacts.bloodType.toLowerCase() == filters.bloodType;
+    } else if (
+      filters.bloodType != "" &&
+      human.knownFacts.bloodType == undefined
+    ) {
+      return false;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.criminalRecord == "yes") {
+      return human.criminalRecord.length > 0;
+    } else if (filters.criminalRecord == "no") {
+      return human.criminalRecord.length == 0;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.fingerprints == "yes") {
+      return human.fingerprints != undefined;
+    } else if (filters.fingerprints == "no") {
+      return human.fingerprints == undefined;
+    }
+
+    return true;
+  });
+
+  var humans = humans.filter(function (human, index) {
+    if (filters.status != "" && human.status != undefined) {
+      return human.status.toLowerCase() == filters.status;
+    }
+
+    return true;
+  });
+
+  humans.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  for (var i = 0; i < humans.length; i++) {
+    let row = document.createElement("tr");
+    row.classList.add("align-middle");
+
+    let img = document.createElement("td");
+    img.innerHTML =
+      '<button class="btn" onclick="window.open(\'' +
+      imgPath +
+      humans[i].img +
+      '\',\'_blank\')"><img class="imgIcon" src="/images/humanDB/imageIcon.png"></button>';
+    row.appendChild(img);
+
+    let name = document.createElement("td");
+    name.classList.add("name");
+    name.innerText = humans[i].name;
+    row.appendChild(name);
+
+    let sex = document.createElement("td");
+    sex.classList.add("sex");
+    sex.innerText = humans[i].sex === undefined ? "" : humans[i].sex;
+    row.appendChild(sex);
+
+    let age = document.createElement("td");
+    age.classList.add("age");
+    age.innerText = humans[i].age === undefined ? "" : humans[i].age;
+    row.appendChild(age);
+
+    let maritalStatus = document.createElement("td");
+    maritalStatus.classList.add("maritalStatus");
+    maritalStatus.innerText =
+      humans[i].knownFacts.maritalStatus === undefined
+        ? ""
+        : humans[i].knownFacts.maritalStatus;
+    row.appendChild(maritalStatus);
+
+    let lastKnownAddress = document.createElement("td");
+    lastKnownAddress.classList.add("lastKnownAddress");
+    lastKnownAddress.innerText =
+      humans[i].knownFacts.lastKnownAddress === undefined
+        ? ""
+        : humans[i].knownFacts.lastKnownAddress;
+    row.appendChild(lastKnownAddress);
+
+    let occupation = document.createElement("td");
+    occupation.classList.add("occupation");
+    occupation.innerText =
+      humans[i].knownFacts.occupation === undefined
+        ? ""
+        : humans[i].knownFacts.occupation;
+    row.appendChild(occupation);
+
+    let criminalRecord = document.createElement("td");
+    criminalRecord.classList.add("criminalRecord");
+    if (humans[i].criminalRecord.length === 0) {
+      criminalRecord.classList.add("green");
+      criminalRecord.innerText = "No";
+    } else {
+      criminalRecord.classList.add("red");
+      criminalRecord.innerText = "Yes";
+    }
+    row.appendChild(criminalRecord);
+
+    let fingerprints = document.createElement("td");
+    fingerprints.classList.add("fingerprints");
+    if (humans[i].fingerprints === undefined) {
+      fingerprints.innerText = "No";
+    } else {
+      fingerprints.classList.add("red");
+      fingerprints.innerText = "Yes";
+    }
+    row.appendChild(fingerprints);
+
+    let status = document.createElement("td");
+    status.classList.add("status");
+    status.innerText = humans[i].status;
+    switch (humans[i].status) {
+      case "Dead":
+        status.classList.add("red");
+        break;
+      case "Alive":
+        status.classList.add("green");
+        break;
+      case undefined:
+        status.classList.add("orange");
+        status.innerText = "";
+        break;
+      default:
+        break;
+    }
+    row.appendChild(status);
+
+    table.appendChild(row);
+  }
 }
 
 function fetchJson(path, filters) {
-    fetch(path)
-    .then(response => response.json())
-    .then(json => fillTable(json.d, filters))
-    .catch(error => console.error('Error fetching JSON:', error))
+  fetch(path)
+    .then((response) => response.json())
+    .then((json) => fillTable(json.d, filters));
 }
 
 function displayHumanDB() {
-    var search = document.getElementById("searchInput").value;
-    var sex = document.getElementById("sex").value;
-    var skin = document.getElementById("skin").value;
-    var eyeColor = document.getElementById("eyeColor").value;
-    var criminalRecord = document.getElementById("criminalRecord").value;
-    var fingerprints = document.getElementById("fingerprints").value;
-    var status = document.getElementById("status").value;
-    
-    fetchJson('data/humanDB.json', { search: search, sex: sex, skin: skin, eyeColor: eyeColor, criminalRecord: criminalRecord, fingerprints: fingerprints, status: status });
+  var search = document.getElementById("searchInput").value;
+  var sex = document.getElementById("sex").value;
+  var skin = document.getElementById("skin").value;
+  var eyeColor = document.getElementById("eyeColor").value;
+  var bloodType = document.getElementById("bloodType").value;
+  var criminalRecord = document.getElementById("criminalRecord").value;
+  var fingerprints = document.getElementById("fingerprints").value;
+  var status = document.getElementById("status").value;
+
+  fetchJson("/data/humanDB.json", {
+    search: search,
+    sex: sex,
+    skin: skin,
+    eyeColor: eyeColor,
+    bloodType: bloodType,
+    criminalRecord: criminalRecord,
+    fingerprints: fingerprints,
+    status: status,
+  });
 }
 
 displayHumanDB();
